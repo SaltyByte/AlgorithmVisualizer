2306df3a244c6dfd616b84c040174bdd23fc0834
Alex <alexnova98@gmail.com>
2021-10-22 14:06:17 +0300

    Adding alittle more code to the recursive maze algorithm, still not working

--- src/algorithms/recursiveMaze.js
+++ src/algorithms/recursiveMaze.js
@@ -4,27 +4,47 @@ function recursiveMaze(grid) {
   let colSize = grid[0].length;
   let rowSize = grid.length;
   const orderedCells = [];
-  recFunc(orderedCells, rowSize, colSize, grid);
+  recMaze(orderedCells, rowSize, colSize, grid);
   return orderedCells;
 }
 
-function recFunc(orderedCells, row, col, grid) {
-  if (row < 2 || col < 2) {
+function recMaze(orderedCells, row, col, grid) {
+  if (row < 4 || col < 4) {
     return;
   }
   // 1 === horizontal, 0 === vertical
   let orientation = Math.floor(Math.random() * 2);
-  wallBuilder(orderedCells, orientation, row, col, grid);
+  console.log("orientation: " + orientation);
+  if (orientation === 1) {
+    let randIndRow = Math.floor(Math.random() * row);
+    wallBuilder(orderedCells, orientation, randIndRow, col, grid);
+    recMaze(orderedCells, randIndRow, col, grid);
+  } else {
+    let randIndCol = Math.floor(Math.random() * col);
+    wallBuilder(orderedCells, orientation, row, randIndCol, grid);
+    recMaze(orderedCells, row, randIndCol, grid);
+  }
 }
 
+// works
 function wallBuilder(orderedCells, orientation, row, col, grid) {
   if (orientation === 1) {
+    let randWallBreakCol = Math.floor(Math.random() * col);
     for (let i = 0; i < col; i++) {
-      orderedCells.push(grid[row][i]);
+      if (randWallBreakCol !== i) {
+        const currCell = grid[row][i];
+        console.log(currCell);
+        orderedCells.push(currCell);
+      }
     }
   } else {
+    let randWallBreakRow = Math.floor(Math.random() * col);
     for (let i = 0; i < row; i++) {
-      orderedCells.push(grid[i][col]);
+      if (randWallBreakRow !== i) {
+        const currCell = grid[i][col];
+        console.log(currCell);
+        orderedCells.push(currCell);
+      }
     }
   }
 }
